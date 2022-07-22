424a7fbe8d26b4a3fb1b78d371feba6536ac2a68
N0Rib <youkd2562@gmail.com>
2022-07-22 12:08:57 +0900

    Initial Commit

---
+++ MyStudy/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MyStudy
+//
+//  Created by 유경덕 on 2022/07/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ MyStudy/MyStudyApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyStudyApp.swift
+//  MyStudy
+//
+//  Created by 유경덕 on 2022/07/22.
+//
+
+import SwiftUI
+
+@main
+struct MyStudyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
